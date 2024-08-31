b0f3ec5ce7b5c840c5ef2247a07c7024bbe9f6e3
Alex Kubica <me@alexkubica.com>
2024-08-31 14:51:59 +0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/',
-  //       headers: [
-  //         {
-  //           key: 'Cross-Origin-Embedder-Policy',
-  //           value: 'require-corp',
-  //         },
-  //         {
-  //           key: 'Cross-Origin-Opener-Policy',
-  //           value: 'same-origin',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
   images: {
     remotePatterns: [
       {
