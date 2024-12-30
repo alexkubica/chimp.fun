a126c08223b2e39072c00e80278954818ac0f25f
Alex Kubica <me@alexkubica.com>
2024-12-30 10:26:14 +0200

    wip

--- app/collectionsMetadata.ts
+++ app/collectionsMetadata.ts
@@ -7,6 +7,7 @@ export const collectionsMetadata: Record<
     cachePath: string;
     total: number;
     contract: string;
+    gifOverride?: (tokenId: string) => string;
   }
 > = {
   chimpers: {
@@ -14,11 +15,15 @@ export const collectionsMetadata: Record<
     cachePath: "chimpers-metadata",
     total: 5555,
     contract: "0x80336ad7a747236ef41f47ed2c7641828a480baa",
+    // gifOverride: (tokenId) =>
+    //   `https://chimp.lon1.cdn.digitaloceanspaces.com/assets/chimpers/pfp-bounce-pixel/${tokenId}.gif`,
   },
   chimpersGenesis: {
     name: "Chimpers Genesis",
     cachePath: "chimpers-genesis-metadata",
     total: 98,
     contract: "0x4d55109a17a6914130ace90325dc98cf66ebfa00",
+    // gifOverride: (tokenId) =>
+    //   `https://chimp.lon1.cdn.digitaloceanspaces.com/assets/genesis/pfp-bounce-pixel/${tokenId}.gif`,
   },
 };
--- app/types.ts
+++ app/types.ts
@@ -1,1 +1,9 @@
 export type CollectionNames = "chimpers" | "chimpersGenesis";
+
+export type ReactionMetadata = {
+  title: string;
+  scale: number;
+  x: number;
+  y: number;
+  filename: string;
+};
