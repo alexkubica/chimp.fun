952713ea03c767c3c16f9056bcc8926e6f89f7ce
Alex Kubica <me@alexkubica.com>
2025-06-07 20:07:46 +0300

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "chimp.fun",
+  icons: {
+    icon: "/icon.gif",
+  },
 };
 
 export default function RootLayout({
