634b1174e7c269b0b1dfdbf06677da2855905ca2
Alex Kubica <me@alexkubica.com>
2025-06-16 19:28:38 +0300

    wip

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -12,18 +12,5 @@ export default function GameLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#000",
-          overflow: "hidden",
-        }}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
